8a2e1fb735c76bd4a65c7525688ffccdc3562d74
hhhaakkkuu <91716496+hhhaakkkuu@users.noreply.github.com>
2022-12-08 13:35:42 +0300

    Add files via upload

---
+++ 14.11-20.11 №2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <ctime> 
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int a[6][4];
+	int k = 0;
+	int min = 1;
+	int max = 10;
+	srand(time(NULL));
+	for (int i = 0; i < 6; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			a[i][j] = min + rand() % (max - min + 1);
+		}
+	}
+	for (int j = 0; j < 4; ++j) {
+		k = 0;
+		for (int i = 0; i < 6; ++i) {
+			if (a[i][j] % 2 == 0)
+				k++;
+		}
+		cout << "for " << j << " k = " << k << endl;
+	}
+	for (int i = 0; i < 6; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			cout << a[i][j] << ' ';
+		}
+		cout << endl;
+	}
+}
