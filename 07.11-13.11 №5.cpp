575bc1189532a203fadf6a78a766a4f1af0c5856
hhhaakkkuu <91716496+hhhaakkkuu@users.noreply.github.com>
2022-12-08 13:30:04 +0300

    Add files via upload

---
+++ 07.11-13.11 №5.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+
+int main() {
+	int temp;
+	int a[5];
+	int min = 1;
+	int max = 30;
+	srand(time(NULL));
+	for (int i = 0; i < 5; i++) {
+		a[i] = min + rand() % (max - min + 1);
+	}
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (a[j] > a[j + 1]) {
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < 5; i++) {
+		cout << a[i] << ' ';
+	}
+	cout << endl << "The second largest element of the given no an empty array of different numbers - " << a[3] << endl;
+	system("pause");
+}
