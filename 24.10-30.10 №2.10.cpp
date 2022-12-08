8cb1781dee648243024941f65772312b134ecff2
hhhaakkkuu <91716496+hhhaakkkuu@users.noreply.github.com>
2022-12-08 13:43:22 +0300

    Add files via upload

---
+++ 24.10-30.10 №2.10.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main(void) {
+    int n;
+    cout << "Enter the number: ";
+TryAgain:
+    cin >> n;
+    if (n == 0 || n > 63) {
+        cout << "Enter correct number: ";
+        goto TryAgain;
+    }
+    cout << '+';
+    for (int i = 0; i < n; i++) {
+        cout << '-';
+    }
+    cout << '+' << endl;
+    for (int i = 0; i < n; i++) {
+        cout << '|';
+        for (int j = 0; j < n; j++) {
+            cout << ' ';
+        }
+        cout << '|' << endl;
+    }
+    cout << '+';
+    for (int i = 0; i < n; i++) {
+        cout << '-';
+    }
+    cout << '+' << endl;
+    return 0;
+}
