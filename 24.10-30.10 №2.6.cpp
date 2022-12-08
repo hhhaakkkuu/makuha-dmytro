a41dfa04346b8c511da6b8a43bd82a75bde2c876
hhhaakkkuu <91716496+hhhaakkkuu@users.noreply.github.com>
2022-12-08 13:46:34 +0300

    Add files via upload

---
+++ 24.10-30.10 №2.6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+    long c0;
+    int counter;
+    while (true) {
+        cin >> c0;
+        while (c0 != 1) {
+            if (c0 % 2 == 0) {
+                c0 = c0 / 2;
+            }
+            else if (c0 % 2 != 0) {
+                c0 = 3 * c0 + 1;
+            }
+            cout << c0 << endl;
+            counter++;
+        }
+        cout << "Steps = ";
+        cout << counter << endl;
+        counter = 0;
+    }
+}
