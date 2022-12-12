5ee06db18cadedafcf1068d2cf6e5333929905b4
hhhaakkkuu <91716496+hhhaakkkuu@users.noreply.github.com>
2022-12-12 09:33:06 +0300

    Add files via upload

---
+++ 28.11-04.12 №1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Date {
+    int hours;
+    int minutes;
+};
+
+
+
+int main() {
+    int time;
+    int t1;
+    Date date = {};
+
+    cin >> date.hours;
+    cin >> date.minutes;
+
+    cin >> time;
+
+    t1 = date.hours * 60 + date.minutes;
+    t1 += time;
+
+    date.hours = t1 / 60;
+    date.minutes = t1 % 60;
+    cout << date.hours << ":" << date.minutes;
+
+    return 0;
+
+}
+#include <iostream>
+
+using namespace std;
+
+struct Date {
+    int hours;
+    int minutes;
+};
