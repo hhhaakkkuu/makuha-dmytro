29b546fefe21cff631b99b148ad6f022574cad1c
hhhaakkkuu <91716496+hhhaakkkuu@users.noreply.github.com>
2022-12-12 09:34:48 +0300

    Add files via upload

---
+++ 28.11-04.12 №2.cpp
@@ -0,0 +1,28 @@
+﻿int main() {
+    int t1, t2;
+    Date date1 = {};
+    Date date2 = {};
+
+
+    cin >> date1.hours;
+    cin >> date1.minutes;
+
+    cin >> date2.hours;
+    cin >> date2.minutes;
+
+    t1 = date1.hours * 60 + date1.minutes;
+    t2 = date2.hours * 60 + date2.minutes;
+
+
+    t1 -= t2;
+
+    if (t1 < 0)
+        t1 = t1 * -1;
+
+    date1.hours = t1 / 60;
+    date1.minutes = t1 % 60;
+    cout << date1.hours << ":" << date1.minutes;
+
+    return 0;
+
+}
